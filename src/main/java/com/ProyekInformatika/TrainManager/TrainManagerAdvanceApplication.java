845a8d02b1a3f75fd1b947f00773c19b92432d6f
Kevin Sunjaya <kssunjaya@gmail.com>
2022-03-06 21:19:16 +0700

    Initial commit

---
+++ src/main/java/com/ProyekInformatika/TrainManager/TrainManagerAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.ProyekInformatika.TrainManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainManagerAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainManagerAdvanceApplication.class, args);
+	}
+
+}
